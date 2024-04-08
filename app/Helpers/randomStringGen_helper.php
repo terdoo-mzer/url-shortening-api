bcab2d8c63f29fbb1d95c912761d9ae8a4a48e3d
Mzer Emmanuel Terdoo <mzerterdoo6@gmail.com>
2024-04-08 10:34:07 +0100

    moved generateRandomString method from the urlController file and created a Helper file in the Helpers directory

---
+++ app/Helpers/randomStringGen_helper.php
@@ -0,0 +1,9 @@
+<?php
+
+function generateRandomString($length = 8)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = substr(str_shuffle($characters), 0, $length);
+
+    return $randomString;
+}
